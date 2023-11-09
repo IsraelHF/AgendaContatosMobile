d36efd721b7ff9c2728667b149c6a5cd76171285
bsampp <b.sampietro02@gmail.com>
2023-11-09 10:50:10 -0300

    Firebase configurado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-   //inserir aqui sua chave
-  }
+  firebaseConfig: {
+    apiKey: 'AIzaSyA6ZMFqWhN1cPNqPwge1pU5K5O5ZEW3TKA',
+    authDomain: 'appcontatos2023tb-e361e.firebaseapp.com',
+    projectId: 'appcontatos2023tb-e361e',
+    storageBucket: 'appcontatos2023tb-e361e.appspot.com',
+    messagingSenderId: '308474319959',
+    appId: '1:308474319959:web:ba4a972b7af5e066bbbf12',
+  },
 };
