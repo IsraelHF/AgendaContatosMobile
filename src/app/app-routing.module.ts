9740d2e46b608be1ad16d04af1752642879a4918
IsraelHF <israel.com.ihf80@gmail.com>
2023-11-30 11:23:43 -0300

    Sign In e Sign Up iniciados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,27 +4,48 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./view/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./view/contatos/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    redirectTo: 'signin',
+    pathMatch: 'full',
   },
   {
     path: 'cadastrar',
-    loadChildren: () => import('./view/cadastrar/cadastrar.module').then( m => m.CadastrarPageModule)
+    loadChildren: () =>
+      import('./view/contatos/cadastrar/cadastrar.module').then(
+        (m) => m.CadastrarPageModule
+      ),
   },
   {
     path: 'detalhar',
-    loadChildren: () => import('./view/detalhar/detalhar.module').then( m => m.DetalharPageModule)
+    loadChildren: () =>
+      import('./view/contatos/detalhar/detalhar.module').then(
+        (m) => m.DetalharPageModule
+      ),
+  },
+  {
+    path: 'signin',
+    loadChildren: () =>
+      import('./view/usuarios/signin/signin.module').then(
+        (m) => m.SigninPageModule
+      ),
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./view/usuarios/signup/signup.module').then(
+        (m) => m.SignupPageModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
