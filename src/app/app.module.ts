56107dd445db5adb1a07b79ae16d6778d268575b
IsraelHF <israel.com.ihf80@gmail.com>
2023-12-07 11:11:19 -0300

    Inicio implementação do Auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,20 @@ import { environment } from 'src/environments/environment.prod';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireStorageModule} from '@angular/fire/compat/storage';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,
-     AngularFireModule.initializeApp(environment.firebaseConfig),
-     AngularFireDatabaseModule,
-     AngularFirestoreModule,
-     IonicModule.forRoot(),
-      AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
